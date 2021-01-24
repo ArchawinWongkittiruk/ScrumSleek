c8e32a382eace2afc19fb8e21e86f6eeca824da6
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-01-24 21:34:01 +0000

    Add loading animation to project page

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -25,7 +25,7 @@ const Project = ({ match }) => {
       <Navbar />
       {!project ? (
         <Box textAlign='center' mt='20%'>
-          <CircularProgress />
+          <CircularProgress isIndeterminate />
         </Box>
       ) : (
         <Box p='1rem'>
