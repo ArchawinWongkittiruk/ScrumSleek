0705ac158e49b6a11ec3950c0a3aa5e35633ff2f
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-02-14 16:58:55 +0000

    Add sprint progress visualisation

--- client/src/components/project/Sprint.js
+++ client/src/components/project/Sprint.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
@@ -7,6 +7,7 @@ import {
   Flex,
   Text,
   Button,
+  Progress,
   Popover,
   PopoverTrigger,
   PopoverContent,
@@ -21,11 +22,26 @@ import Task from './Task';
 dayjs.extend(relativeTime);
 
 const Sprint = () => {
+  const [progress, setProgress] = useState(0);
   const tasks = useSelector((state) =>
     state.project.project.tasks.filter((task) => task.location === 'SPRINT')
   );
+  const statuses = useSelector((state) => state.project.project.statuses);
   const sprint = useSelector((state) => state.project.project.sprint);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    setProgress(
+      (tasks.reduce(
+        (total, task) =>
+          total +
+          statuses.findIndex((status) => status._id === task.status) / (statuses.length - 1),
+        0
+      ) /
+        tasks.length) *
+        100
+    );
+  }, [tasks, statuses]);
 
   const onEndSprint = async (e) => {
     e.preventDefault();
@@ -35,23 +51,31 @@ const Sprint = () => {
   return (
     <>
       <Flex justify='space-between' wrap='wrap' pb='1rem'>
-        <Text fontSize='xl'>Sprint</Text>
+        <Text fontSize='xl'>Sprint Progress</Text>
         <Popover>
           <PopoverTrigger>
-            <Button colorScheme='red'>End Sprint</Button>
+            <Button colorScheme={progress !== 100 ? 'red' : 'green'}>End Sprint</Button>
           </PopoverTrigger>
           <PopoverContent mr='1.5rem'>
             <PopoverArrow />
             <PopoverCloseButton />
             <PopoverHeader>Are You Sure?</PopoverHeader>
             <PopoverBody>
-              <Button onClick={onEndSprint} colorScheme='red'>
+              <Button onClick={onEndSprint} colorScheme={progress !== 100 ? 'red' : 'green'}>
                 Yes, End the Sprint
               </Button>
             </PopoverBody>
           </PopoverContent>
         </Popover>
       </Flex>
+      <Progress
+        value={progress}
+        mb='1rem'
+        w='100%'
+        colorScheme={
+          progress === 100 ? 'green' : progress < 33 ? 'red' : progress < 66 ? 'orange' : 'yellow'
+        }
+      />
       <Flex wrap='wrap' pb='1rem'>
         <Text pr='1rem'>
           Start: {dayjs(sprint.start).format('DD/MM/YYYY HH:mm')} ({dayjs(sprint.start).fromNow()})
