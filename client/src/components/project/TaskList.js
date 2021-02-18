4d2ffee02be4f7dc169c5a42d732202d6c9e5b5b
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-02-18 20:24:38 +0000

    Switch from react-sortable-hoc to dnd-kit

--- client/src/components/project/TaskList.js
+++ client/src/components/project/TaskList.js
@@ -1,47 +1,76 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from '@dnd-kit/core';
+import {
+  SortableContext,
+  sortableKeyboardCoordinates,
+  rectSortingStrategy,
+} from '@dnd-kit/sortable';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 import { moveTask } from '../../actions/tasks';
 import { Flex } from '@chakra-ui/react';
 
 import CreateTask from './CreateTask';
 import Task from './Task';
 
-const SortableItem = SortableElement(({ task }) => <Task task={task} />);
+const SortableItem = ({ task }) => {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    id: task._id,
+  });
 
-const SortableList = SortableContainer(({ allTasks, tasks, canCreateTask }) => {
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
   return (
-    <Flex wrap='wrap' pt='1rem'>
-      {tasks.map((task) => (
-        <SortableItem
-          key={task._id}
-          index={allTasks.findIndex((allTask) => allTask._id === task._id)}
-          task={task}
-        />
-      ))}
-      {canCreateTask && <CreateTask />}
-    </Flex>
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      <Task task={task} />
+    </div>
   );
-});
+};
 
 const TaskList = ({ tasks, canCreateTask }) => {
   const allTasks = useSelector((state) => state.project.project.tasks);
   const dispatch = useDispatch();
 
-  const onSortEnd = ({ oldIndex, newIndex }) => {
-    const task = allTasks[oldIndex];
-    dispatch(moveTask(task._id, { oldIndex, newIndex }));
-  };
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      const oldIndex = allTasks.findIndex((task) => task._id === active.id);
+      const newIndex = allTasks.findIndex((task) => task._id === over.id);
+      dispatch(moveTask(active.id, { oldIndex, newIndex }));
+    }
+  }
 
   return (
-    <SortableList
-      tasks={tasks}
-      onSortEnd={onSortEnd}
-      axis='xy'
-      allTasks={allTasks}
-      canCreateTask={canCreateTask}
-    />
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <SortableContext items={tasks.map((task) => task._id)} strategy={rectSortingStrategy}>
+        <Flex wrap='wrap' pt='1rem'>
+          {tasks.map((task) => (
+            <SortableItem key={task._id} task={task} />
+          ))}
+          {canCreateTask && <CreateTask />}
+        </Flex>
+      </SortableContext>
+    </DndContext>
   );
 };
 
