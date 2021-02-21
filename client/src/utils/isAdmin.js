22db64f2fd0cf32386a3556824be62d0da7c1786
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-02-21 16:54:40 +0000

    Fix isAdmin check

--- client/src/utils/isAdmin.js
+++ client/src/utils/isAdmin.js
@@ -1,7 +1,7 @@
 const isAdmin = (project, user) => {
   if (!project || !user) return false;
-  return !project.members
-    .filter((member) => member.role !== 'admin')
+  return project.members
+    .filter((member) => member.role === 'admin')
     .map((member) => member.user)
     .includes(user._id);
 };
