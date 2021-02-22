75aeb92d260f814989b09e72840595f3d4f84954
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-02-22 23:44:45 +0000

    Display only finished story points in Completed

--- client/src/components/project/StoryPoints.js
+++ client/src/components/project/StoryPoints.js
@@ -14,13 +14,15 @@ const StoryPoints = ({ location, sprintId }) => {
     <Flex alignItems='center'>
       <Text>Story Points -</Text>
       <Flex wrap='wrap'>
-        {statuses.map((status) => (
-          <Badge key={status._id} colorScheme={status.color} ml='0.4rem'>
-            {tasks
-              .filter((task) => task.status === status._id)
-              .reduce((total, task) => total + task.storyPoints, 0)}
-          </Badge>
-        ))}
+        {(location !== 'COMPLETED' ? statuses : statuses.slice(statuses.length - 1)).map(
+          (status) => (
+            <Badge key={status._id} colorScheme={status.color} ml='0.4rem'>
+              {tasks
+                .filter((task) => task.status === status._id)
+                .reduce((total, task) => total + task.storyPoints, 0)}
+            </Badge>
+          )
+        )}
       </Flex>
     </Flex>
   );
