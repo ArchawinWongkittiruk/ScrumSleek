078ebf6121bb0a7e4a94465b18c9247ae9309a7a
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-03-01 23:10:50 +0000

    Add completed sprints page pagination

--- client/src/components/pages/Completed.js
+++ client/src/components/pages/Completed.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Button } from '@chakra-ui/react';
 
 import StoryPoints from '../project/StoryPoints';
 import CompletedSprint from '../project/CompletedSprint';
 
 const Completed = () => {
+  const [amount, setAmount] = useState(5);
   const sprints = useSelector((state) => state.project.project.sprints);
   const sprintOngoing = useSelector((state) => state.project.project.sprintOngoing);
-  const completedSprints = sprintOngoing ? sprints.slice(1) : sprints;
+  const completedSprints = sprintOngoing ? sprints.slice(1, amount + 1) : sprints.slice(0, amount);
 
   return (
     <>
@@ -16,13 +17,20 @@ const Completed = () => {
         <Text fontSize='xl'>Completed</Text>
         <StoryPoints location='COMPLETED' />
       </Flex>
-      {completedSprints.map((sprint, index) => (
+      {completedSprints.map((completedSprint) => (
         <CompletedSprint
-          key={sprint._id}
-          sprint={sprint}
-          number={completedSprints.length - index}
+          key={completedSprint._id}
+          sprint={completedSprint}
+          number={
+            sprints.length - sprints.findIndex((sprint) => sprint._id === completedSprint._id)
+          }
         />
       ))}
+      {sprints.length > 5 && (
+        <Button onClick={() => setAmount(amount + 5)} isDisabled={amount > sprints.length - 1}>
+          View More Sprints
+        </Button>
+      )}
     </>
   );
 };
