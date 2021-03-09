e3577d69b275b16db3f04b7de4b9fe2d702b8b19
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-03-09 22:21:18 +0000

    Set socket io endpoint dynamically on NODE_ENV

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,9 @@
 import io from 'socket.io-client';
 
-const socket = io('https://scrumsleek.herokuapp.com/');
+const socket = io(
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:5000/'
+    : 'https://scrumsleek.herokuapp.com/'
+);
 
 export default socket;
