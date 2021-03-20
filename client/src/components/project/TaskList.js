eea8bbd1389a7d0dd802727b751d1b60ca2656a5
ArchawinWongkittiruk <willypoko@hotmail.com>
2021-03-20 21:29:36 +0000

    Refactor TaskList sorts

--- client/src/components/project/TaskList.js
+++ client/src/components/project/TaskList.js
@@ -21,56 +21,35 @@ import { ChevronDownIcon } from '@chakra-ui/icons';
 import CreateTask from './CreateTask';
 import Task from './Task';
 
-const sortTypes = [
-  'Default',
-  'Title',
-  'Label',
-  'Story Points',
-  'Members',
-  'Status',
-  'Most Recently Created',
-  'Least Recently Created',
-  'Most Recently Updated',
-  'Least Recently Updated',
-];
-
-const getSort = (type, statuses, a, b) => {
-  switch (type) {
-    case 'Title':
-      return a.title.localeCompare(b.title);
-    case 'Label':
-      return a.label.localeCompare(b.label);
-    case 'Story Points':
-      return b.storyPoints - a.storyPoints;
-    case 'Members':
-      return b.members.length - a.members.length;
-    case 'Status':
-      return (
-        statuses.findIndex((status) => status._id === a.status) -
-        statuses.findIndex((status) => status._id === b.status)
-      );
-    case 'Most Recently Created':
-      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
-    case 'Least Recently Created':
-      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
-    case 'Most Recently Updated':
-      return new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime();
-    case 'Least Recently Updated':
-      return new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime();
-    default:
-      return 0;
-  }
-};
-
 const TaskList = ({ location, sprintId, canCreateTask }) => {
   const [sortType, setSortType] = useState('Default');
   const allTasks = useSelector((state) => state.project.project.tasks);
   const statuses = useSelector((state) => state.project.project.statuses);
+  const dispatch = useDispatch();
+
+  const sorts = {
+    Default: () => 0,
+    Title: (a, b) => a.title.localeCompare(b.title),
+    Label: (a, b) => a.label.localeCompare(b.label),
+    'Story Points': (a, b) => b.storyPoints - a.storyPoints,
+    Members: (a, b) => b.members.length - a.members.length,
+    Status: (a, b) =>
+      statuses.findIndex((status) => status._id === a.status) -
+      statuses.findIndex((status) => status._id === b.status),
+    'Most Recently Created': (a, b) =>
+      new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime(),
+    'Least Recently Created': (a, b) =>
+      new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+    'Most Recently Updated': (a, b) =>
+      new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime(),
+    'Least Recently Updated': (a, b) =>
+      new Date(a.updatedAt).getTime() - new Date(b.updatedAt).getTime(),
+  };
+
   const tasks = (sprintId
     ? allTasks.filter((task) => task.sprintCompleted === sprintId)
     : allTasks.filter((task) => task.location === location)
-  ).sort((a, b) => getSort(sortType, statuses, a, b));
-  const dispatch = useDispatch();
+  ).sort(sorts[sortType]);
 
   const sensors = useSensors(
     useSensor(PointerSensor),
@@ -96,7 +75,7 @@ const TaskList = ({ location, sprintId, canCreateTask }) => {
           Sort By: {sortType}
         </MenuButton>
         <MenuList>
-          {sortTypes.map((type) => (
+          {Object.keys(sorts).map((type) => (
             <MenuItem key={type} onClick={() => setSortType(type)} isDisabled={sortType === type}>
               {type}
             </MenuItem>
